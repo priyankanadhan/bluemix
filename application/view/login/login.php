9cd2667d0d3e12a1d00e3e75239c0f2e51518e81
priyananadhan <priyankaece146@gmail.com>
2016-02-23 14:03:53 +0530

    Updated the files for google login

--- application/view/login/login.php
+++ application/view/login/login.php
@@ -204,7 +204,7 @@
 				</form>
 
 				<div class="external-login" style="width: 70%; padding-left: 25%;color:red;">
-					<a href=<?php echo $authurl;?> class="gplus"> <i class="fa-google-plus"></i> Login
+					<a href="login/gplusLogin" class="gplus"> <i class="fa-google-plus"></i> Login
 						with Google Plus
 					</a>
 
